889ffb7e3b245d7967ba05d0c0f4c52c7d0cecd8
TheRemiH <118737325+TheRemiH@users.noreply.github.com>
2024-10-02 20:38:58 +0200

    return list of string

--- google.py
+++ google.py
@@ -3,4 +3,4 @@ pytrend = TrendReq()
 
 # Get the top 20 trending Google searches for a given country (default: USA)
 def get_google_trends(cn='united_states'):
-    return pytrend.trending_searches(pn=cn)
+    return pytrend.trending_searches(pn=cn)[0].values.tolist()
